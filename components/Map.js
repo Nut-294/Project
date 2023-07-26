d5d40c496e4df8cfe11ccaa9a3829eea14b51e81
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2023-07-26 16:09:05 +0700

    createmap-pageone

---
+++ components/Map.js
@@ -0,0 +1,19 @@
+import { MapContainer,Marker,TileLayer,Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"
+function Map() {
+  return (
+    <MapContainer style={{height:"450px",width:"1000px",}} center={[13.736717, 100.523186]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          {/* A pretty CSS3 popup. <br /> Easily customizable. */}
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
--- pages/PageOne.js
+++ pages/PageOne.js
@@ -1,13 +1,17 @@
-"use client"
 import React from 'react'
 import SideBar from '@/components/SideBar'
+import dynamic from 'next/dynamic'
+
+const DynamicMap = dynamic(() => import('../components/Map'), {
+  loading: () => <p>Loading...</p>,ssr:false,
+})
 function PageOne() {
   return (
-    <div className='flex'>
-    <SideBar/>
-    <div className='text-white'>
-      <h1>PageOne</h1>
-    </div>
+   <div className='flex'>
+    <SideBar />
+      <div className='ml-48 mt-20'>
+      <DynamicMap/>
+      </div>
     </div>
   )
 }
