0c5d98fb473f0bdc9ec5440e93225f67a7f3887c
natcha <63010291@kmitl.ac.th>
2023-08-11 21:06:15 +0700

    far 11/8/2566

---
+++ app/api/sitehistorical/route.js
@@ -0,0 +1,17 @@
+//url: http://localhost:3000/api/position
+import prisma from "@/libs/prismadb"
+import { NextResponse } from "next/server"
+
+export const GET = async () => {
+    try{
+        
+        const sitehistoricals = await prisma.sitehistorical.findMany({
+            take:100
+        })
+
+        return NextResponse.json(sitehistoricals)
+
+    }catch(err){
+        return NextResponse.json({message: "GET Error", err}, {status: 500})
+    }
+}
