64254233962d0839655a446fe03fc01643c5e538
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2023-09-19 23:01:16 +0700

    nav

--- components/compoTE4/bar/Home.js
+++ components/compoTE4/bar/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="fixed z-40">
       <div className="w-screen h-20 bg-slate-600 flex  items-center border-2  border-black pl-8 py-8">
         <div className="text-4xl text-white font-medium">
-          Signal
+          Home
         </div>
       </div>
     </div>
