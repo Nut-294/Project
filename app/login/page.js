9d3de07eaca495990ce55a03e3425018ce70a35f
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2023-09-24 17:28:24 +0700

    ccc

--- app/login/page.js
+++ app/login/page.js
@@ -40,11 +40,11 @@ export default function Login() {
     }
 
     return (
-    <div className='w-full min-h-screen flex justify-center items-center bg-gray-900'>
-        <div className='relative w-[380px] h-[420px] bg-gray-800 rounded-lg overflow-hidden'>
+    <div className='w-full min-h-screen flex justify-center items-center bg-gray-800'>
+        <div className='relative w-[380px] h-[420px] bg-gray-700 rounded-lg overflow-hidden'>
             <div className='absolute w-[380px] h-[420px] bg-gradient-to-r from-teal-400 via-teal-400 to-transparent -top-[50%] -left-[50%] animate-spin-slow origin-bottom-right'></div>
             <div className='absolute w-[380px] h-[420px] bg-gradient-to-r from-teal-400 via-teal-400 to-transparent -top-[50%] -left-[50%] animate-spin-delay origin-bottom-right'></div>
-            <div className='absolute inset-1 bg-gray-800 rounded-lg z-10 p-5'>
+            <div className='absolute inset-1 bg-gray-700 rounded-lg z-10 p-5'>
                 <form onSubmit={handleSubmit}>
                     <h2 className='text-xl font-semibold text-teal-400 text-center mb-12 mt-6'>Sign in</h2>
                     <div className='relative flex flex-col my-12'>
